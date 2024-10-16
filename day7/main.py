b4feabcabc31d229d545bcdd48fb86911135b72a
Bui Hai Duc <ducbanca1604@gmail.com>
2024-10-16 10:34:44 +0700

    fix you lose

--- day7/main.py
+++ day7/main.py
@@ -43,33 +43,34 @@ from hangman_art import logo
 from hangman_art import stages
 from hangman_word import word_list
 import random
+
+
 print(logo)
 choosen  = random.choice(word_list)
-print(f"the solution is {choosen}")
 list = []
 for letter in choosen:
     list += '_'
 number_stages = 6
 end_game = False
+
+
 while not end_game:
     guess = input("Guess a letter: ").lower()
     for position in range(len(choosen)):
         letter = choosen[position]
         if letter == guess:
             list[position] = letter
-
     if guess not in list:
         number_stages -= 1
         if number_stages == 0:
             end_game = True
-            print("You Lose!")
-
+    
     print(stages[number_stages])
     print(f"{' '.join(list)}")
+
+    if number_stages == 0:
+        print("You Lose!")
 
     if '_' not in list:
         end_game = True
         print("You Win!")
-    elif number_stages == 0:
-        end_game = True
-        print("You Lose!")
